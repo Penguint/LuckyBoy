96a8a26b067cb687b776fd77d2553102f04b3fda
Penguint <tjpyytqz@outlook.com>
2018-11-25 01:27:08 +0800

    Get initial list

---
+++ script.js
@@ -0,0 +1,11 @@
+function getJSON(url){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "url", true);
+    xhr.send();
+
+    return(xhr.responseText);
+}
+
+window.onload = function() {
+    console.log(getJSON("./init.json"));
+}
