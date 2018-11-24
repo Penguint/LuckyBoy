12881d0e0ebd43b67b683a1ca3571606e552fdd9
Penguint <tjpyytqz@outlook.com>
2018-11-25 02:54:21 +0800

    return json

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getJSON(url, callback) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            callback(xhr.responseText);
+            callback(JSON.parse(xhr.responseText));
         }
     };
     xhr.open("GET", url, true);
@@ -11,7 +11,7 @@ function getJSON(url, callback) {
 }
 
 window.onload = function () {
-    getJSON("init.json", (text) => {
-        console.log(text);
+    getJSON("init.json", (json) => {
+        console.log(json);
     });
 }
