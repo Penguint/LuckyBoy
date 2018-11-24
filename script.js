b0daf2e8882bbb83cafef0c865ccfe4cec393671
Penguint <tjpyytqz@outlook.com>
2018-11-25 02:46:27 +0800

    Check request status

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function getJSON(url, callback){
+function getJSON(url, callback) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
-        callback(xhr.responseText);
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(xhr.responseText);
+        }
     };
     xhr.open("GET", url, true);
     xhr.send();
 
 }
 
-window.onload = function() {
+window.onload = function () {
     getJSON("init.json", (text) => {
         console.log(text);
     });
